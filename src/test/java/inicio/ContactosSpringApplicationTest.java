95b399ff17442cd898d59936ee0413488e66b3b4
JPortugal <jose.portugal@stucom.com>
2024-05-10 16:25:53 +0200

    first commit

---
+++ src/test/java/inicio/ContactosSpringApplicationTest.java
@@ -0,0 +1,14 @@
+package inicio;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class ContactosSpringApplicationTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
